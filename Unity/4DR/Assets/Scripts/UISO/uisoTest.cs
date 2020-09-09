21789f76d5947ac022239193a5226aac28f83530
lejail <lejail@lejail.com>
2020-09-09 16:07:35 +0900

    day over

---
+++ Unity/4DR/Assets/Scripts/UISO/uisoTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class uisoTest : MonoBehaviour
+{
+
+    public TweenPosition tweenPosiiton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void OnFfff() {
+
+        tweenPosiiton.ResetToBeginning();
+        tweenPosiiton.PlayForward();
+    }
+}
